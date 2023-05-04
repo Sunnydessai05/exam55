5a641fe397a922167f50f556ae3ce27f40e01bee
exam <exam@Bcalab4-m12>
2023-05-04 12:11:43 +0530

    this is my exam question

---
+++ CaseToolsExam/src/PiggyBank_236.java
@@ -0,0 +1,15 @@
+
+public class PiggyBank_236 {
+	int num_coins;
+	int coin_value;
+	PiggyBank_236()
+	{
+		this.num_coins=num_coins;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
